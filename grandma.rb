004d2b01e1f1896274b14373317f6288cc18ebd1
Shelly Chen <shellyjychen@gmail.com>
2018-10-25 01:52:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    return "HUH?! SPEAK UP SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
