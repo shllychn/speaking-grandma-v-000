4aefcb182e00011b2f2ae7e44c42fd24b3f790cf
Shelly Chen <shellyjychen@gmail.com>
2018-10-25 01:52:22 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+  if phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  else
-    puts "HUH?! SPEAK UP SONNY!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
